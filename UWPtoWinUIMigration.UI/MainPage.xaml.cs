df92f11d0a5faccef504ac9ace1dd0e6edd9c80a
Nick Randolph <nick@builttoroam.com>
2021-02-11 22:24:48 +1100

    Add project files.

---
+++ UWPtoWinUIMigration.UI/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿#if WinUI
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+#else
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+#endif
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPtoWinUIMigration.UI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void NavigateToSecondPageClick(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(SecondPage));
+        }
+    }
+}
